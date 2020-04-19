e7a9ff6fd9010412aa382319beebc87019aac4c0
fChristenson <fredrik.christenson@gmail.com>
2020-04-19 21:21:14 +0200

    wp

--- src/UserModel.js
+++ src/UserModel.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  firstName: String,
+  lastName: String,
   name: String,
   age: Number,
-  email: String,
+  //email: String,
   createdAt: { type: Date, default: Date.now },
 });
 
--- src/app.js
+++ src/app.js
@@ -11,9 +11,42 @@ app.post("/api/v1/users", async (req, res) => {
   res.json(user);
 });
 
+app.get("/api/v1/users", async (req, res) => {
+  const users = await User.find({});
+
+  res.json(users);
+});
+
+// Adding a property creates a inconsistent model
+// We can solve this problem with a migration job
 app.post("/api/v2/users", async (req, res) => {
   const { name, age, email } = req.body;
   const user = await new User({ name, age, email }).save();
+
+  res.json(user);
+});
+
+// In version 3 we push models with an updated structure as well.
+// But what if we can't run a migration job?
+app.post("/api/v3/users", async (req, res) => {
+  const { firstName, lastName, age, email } = req.body;
+  const user = await new User({ firstName, lastName, age, email }).save();
+
+  res.json(user);
+});
+
+// If we can't stop the system to run a migraiton job a slower migration strategy is needed.
+// The following code is an example of migrating each user as they use the system.
+app.get("/api/v3/users/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user.name) {
+    const [first, last] = user.name.split(" ");
+    user.firstName = first;
+    user.lastName = last || "";
+    user.name = undefined;
+    await user.save();
+  }
 
   res.json(user);
 });
