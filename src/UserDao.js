dd86cebe0e496bc4217a5949ed14056e3e337acf
fChristenson <fredrik.christenson@gmail.com>
2020-04-20 06:19:28 +0200

    wp

---
+++ src/UserDao.js
@@ -0,0 +1,29 @@
+const User = require("./UserModel");
+
+class UserDao {
+  findAll() {
+    return await User.find({});
+  }
+
+  async findById(id) {
+    const user = await User.findById(id);
+
+    // If the user has to provide new data we can't auto rewrite like this.
+    if (user.name) {
+      const [first, last] = user.name.split(" ");
+      user.firstName = first;
+      user.lastName = last || "";
+      user.name = undefined;
+      await user.save();
+    }
+
+    return user;
+  }
+
+  saveUser(props) {
+    const { firstName, lastName, age, email } = props;
+    new User({ firstName, lastName, age, email }).save();
+  }
+}
+
+module.exports = new UserDao();
